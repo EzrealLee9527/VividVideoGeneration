70b07c5b32df69d35891677bc44b250522c67bc7
jingminhao <jingminhao666@gmail.com>
2023-12-25 08:31:39 +0000

    Update README.md

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Finetuning SVD with Diffusers+Accelerate .
 # News
+**2023.12.25**
+- support for FSDP now. I test on SDP configure: accelerate_configs/8gpu_fp16_sdp_config.yaml
+
 **2023.12.22**
 - support for fine-tuning Image2Video SVD. I will record a detailed worklog in https://wiki.megvii-inc.com/pages/viewpage.action?pageId=523140390 later.
 
@@ -7,4 +10,5 @@
 - conda ：environment.yaml
 
 # Train:
-`bash ./train.sh accelerate_configs/8gpu_fp16_config.yaml`
+`bash ./train.sh accelerate_configs/8gpu_fp16_sdp_config.yaml` for fsdp
+`bash ./train.sh accelerate_configs/8gpu_fp16_config.yaml` for ddp
--- train.sh
+++ train.sh
@@ -1,6 +1,8 @@
-export MODEL_NAME="stabilityai/stable-video-diffusion-img2vid"
+# export MODEL_NAME="stabilityai/stable-video-diffusion-img2vid"
+export MODEL_NAME="/data/users/jingminhao/.cache/huggingface/hub/models--stabilityai--stable-video-diffusion-img2vid/snapshots/0f2d55c1e358d608120344d3ea9c35fb5f2c31b3"
 ABSOLUTE_DIRNAME=`pwd`
-EXPNAME=`basename $ABSOLUTE_DIRNAME`
+# EXPNAME=`basename $ABSOLUTE_DIRNAME`
+EXPNAME=SVD_I2V_SDP
 
 while1(){
   while true;do
@@ -15,17 +17,17 @@ while1(){
 # 数据:OpenImages + InstanceSegMask
 accelerate launch --config_file $1 train_svd_condition_image.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
-  --per_gpu_batch_size=1 \
+  --per_gpu_batch_size=2 \
   --gradient_accumulation_steps=4 \
   --max_train_steps=1000000 \
   --learning_rate=1e-04 \
   --max_grad_norm=1 \
   --lr_scheduler="constant" \
-  --lr_warmup_steps=500 \
+  --lr_warmup_steps=1000 \
   --output_dir="/dev/shm/"$EXPNAME"/saved" \
   --num_workers=8 \
   --checkpoints_total_limit=3 \
-  --checkpointing_steps=1000 \
+  --checkpointing_steps=500 \
   --resume_from_checkpoint="latest" \
   --enable_xformers_memory_efficient_attention \
   --validation_steps=500 \
