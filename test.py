a93c936b8f9de89d097768618d1cf7cb352ee4b0
jingminhao <jingminhao666@gmail.com>
2023-12-28 02:57:14 +0000

    ...

--- test.py
+++ test.py
@@ -1,56 +1,102 @@
 import torch
-
+import megfile
 from diffusers import StableVideoDiffusionPipeline
 from diffusers.utils import load_image, export_to_video
 from PIL import Image
 import numpy as np
+from diffusers import UNetSpatioTemporalConditionModel
+
+def setup_pipeline(args):
+    pipe = StableVideoDiffusionPipeline.from_pretrained(
+        args.pretrained_model_name_or_path, 
+        torch_dtype=torch.float16, variant="fp16"
+        #"stabilityai/stable-video-diffusion-img2vid", torch_dtype=torch.float16, variant="fp16"
+    )
+    if args.unet_path:
+        # unet = UNetSpatioTemporalConditionModel.from_pretrained(args.unet_path,)
+        # pipe.unet = unet
+
+        with megfile.smart_open(args.unet_path, "rb") as rbf:
+            state_dict = torch.load(rbf)
+            import pdb; pdb.set_trace()
+            pipe.unet.load_state_dict(state_dict)
+            # pipe.unet = pipe.unet.eval()
+    pipe.enable_model_cpu_offload()
+    # pipe.to("cuda") # it is not necessary to move pipe to GPU manually if enable model_cpu_offload, because the pipe will be moved automatically.
+
+    # import pdb; pdb.set_trace()
+    return pipe
 
 
-def export_to_gif(frames, output_gif_path, fps):
-    """
-    Export a list of frames to a GIF.
+def run(args):
+    pipe = setup_pipeline(args)
+    
+    # Load the conditioning image
+    image = load_image("images/miaoji-female-americahighschool.png")
+    image = image.resize((args.width, args.height))
 
-    Args:
-    - frames (list): List of frames (as numpy arrays or PIL Image objects).
-    - output_gif_path (str): Path to save the output GIF.
-    - duration_ms (int): Duration of each frame in milliseconds.
+    generator = torch.manual_seed(99)
 
-    """
-    # Convert numpy arrays to PIL Images if needed
-    pil_frames = [Image.fromarray(frame) if isinstance(
-        frame, np.ndarray) else frame for frame in frames]
+    frames = pipe(
+        image, 
+        generator=generator, 
+        height=args.height, 
+        width=args.width,
+        num_inference_steps=args.num_inference_steps
+    ).frames[0]
 
-    pil_frames[0].save(output_gif_path.replace('.mp4', '.gif'),
-                       format='GIF',
-                       append_images=pil_frames[1:],
-                       save_all=True,
-                       duration=500,
-                       loop=0)
-
-pipe = StableVideoDiffusionPipeline.from_pretrained(
-    "/data/users/jingminhao/.cache/huggingface/hub/models--stabilityai--stable-video-diffusion-img2vid/snapshots/0f2d55c1e358d608120344d3ea9c35fb5f2c31b3", torch_dtype=torch.float16, variant="fp16"
-    #"stabilityai/stable-video-diffusion-img2vid", torch_dtype=torch.float16, variant="fp16"
-)
-pipe.enable_model_cpu_offload()
-
-# Load the conditioning image
-image = load_image("images/gaoyuanyuan.png")
-image = image.resize((512, 320))
-
-generator = torch.manual_seed(99)
-frames = pipe(image, generator=generator).frames[0]
-
-# export_to_video(frames, "generated.mp4", decode_chunk_size=8, fps=7)
+    frames[0].save(
+        "gifs/svd_i2v_sdp/miaoji-female-americahighschool.gif",
+        format='GIF',
+        append_images=frames[1:],
+        save_all=True,
+        duration=500,
+        loop=0
+    )
 
 
-frames[0].save(
-    "generated.gif",
-    format='GIF',
-    append_images=frames[1:],
-    save_all=True,
-    duration=500,
-    loop=0
-)
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(
+        description="Script to train Stable Diffusion XL for InstructPix2Pix."
+    )
+    parser.add_argument(
+        "--pretrained_model_name_or_path",
+        type=str,
+        default=None,
+        required=True,
+        help="Path to pretrained model or model identifier from huggingface.co/models.",
+    )
+    parser.add_argument(
+        "--unet_path",
+        type=str,
+        default=None,
+        required=False,
+        help="Path to pretrained model or model identifier from huggingface.co/models.",
+    )
+    parser.add_argument(
+        "--revision",
+        type=str,
+        default=None,
+        required=False,
+        help="Revision of pretrained model identifier from huggingface.co/models.",
+    )
+    parser.add_argument(
+        "--width",
+        type=int,
+        default=1024,
+    )
+    parser.add_argument(
+        "--height",
+        type=int,
+        default=576,
+    )
+    parser.add_argument(
+        "--num_inference_steps",
+        type=int,
+        default=25,
+    )
 
-print("....")
-# import hf_transfer
+    args = parser.parse_args()
+
+    run(args)
