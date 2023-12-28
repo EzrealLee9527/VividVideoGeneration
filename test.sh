1de12d184c15a3e3863d87d795cf19b964efcd58
jingminhao <jingminhao666@gmail.com>
2023-12-28 02:56:55 +0000

    update test.py

---
+++ test.sh
@@ -0,0 +1,6 @@
+python3 test_bak.py\
+    --pretrained_model_name_or_path "/data/users/jingminhao/.cache/huggingface/hub/models--stabilityai--stable-video-diffusion-img2vid/snapshots/0f2d55c1e358d608120344d3ea9c35fb5f2c31b3"\
+    --unet_path "/data/users/jingminhao/pytorch_model_fsdp.bin"\
+    --width 1024\
+    --height 576\
+    --num_inference_steps 25
